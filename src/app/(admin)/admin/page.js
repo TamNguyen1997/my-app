ce1cf95e177ff51836e341bf674197536803a0bd
TamNguyen1997 <tam070397@gmail.com>
2024-06-18 22:12:29 +0700

    Update stuff

--- src/app/(admin)/admin/page.js
+++ src/app/(admin)/admin/page.js
@@ -1,49 +1,6 @@
-import { Listbox, ListboxItem } from "@nextui-org/react";
-import Link from "next/link";
-
-const menu = [
-  {
-    id: "image",
-    link: "/admin/image",
-    name: "Gallery"
-  },
-  {
-    id: "category",
-    link: "/admin/category",
-    name: "Category"
-  },
-  {
-    id: "product",
-    link: "/admin/product",
-    name: "Sản phẩm"
-  },
-  {
-    id: "blog",
-    link: "/admin/blog",
-    name: "Blog"
-  }
-]
-
 const Admin = () => {
   return (
     <div>
-      <div className="flex justify-between min-h-screen">
-        <div className="flex flex-col gap-2 w-1/6 border-r min-h-full p-2">
-          <div className="border-small px-1 py-2 rounded-small border-default-200 shadow-md">
-            <Listbox
-              aria-label="Single selection example"
-              variant="flat">
-              {
-                menu.map(item => {
-                  return <ListboxItem key={item.id}>
-                    <Link href={item.link}>{item.name}</Link>
-                  </ListboxItem>
-                })
-              }
-            </Listbox>
-          </div>
-        </div>
-      </div>
     </div>
   );
 };
--- src/app/components/admin/ui/Sidebar.js
+++ src/app/components/admin/ui/Sidebar.js
@@ -2,16 +2,19 @@ import { Image, NotebookPen, ShoppingBasket } from 'lucide-react';
 
 const items = [
   {
+    id: 'gallery',
     name: 'Gallery',
     icon: <Image />,
     link: '/admin/image'
   },
   {
+    id: "product",
     name: 'Sản phẩm',
     icon: <ShoppingBasket />,
     link: '/admin/product'
   },
   {
+    id: "blog",
     name: 'Blog',
     icon: <NotebookPen />,
     link: '/admin/blog'
@@ -25,7 +28,7 @@ export default () => {
         <ul className="space-y-2 font-medium">
           {
             items.map(item => {
-              return <li>
+              return <li key={item.id}>
                 <a href={item.link} className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-500 group">
                   {item.icon}
                   <span className="ms-3">{item.name}</span>

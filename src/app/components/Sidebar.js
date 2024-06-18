73fd830d1679fb9cd3ca43dec122465632e1212f
TamNguyen1997 <tam070397@gmail.com>
2024-06-18 22:08:50 +0700

    Fix sidebar

--- src/app/components/Sidebar.js
+++ src/app/components/Sidebar.js
@@ -2,40 +2,24 @@ import { Listbox, ListboxItem } from "@nextui-org/react";
 import Link from "next/link";
 
 const Sidebar = () => {
-  const menu = [
-    {
-      id: "category",
-      link: "/admin/category",
-      name: "Category"
-    },
-    {
-      id: "product",
-      link: "/admin/product",
-      name: "Sản phẩm"
-    },
-    {
-      id: "blog",
-      link: "/admin/blog",
-      name: "Blog"
-    }
-  ]
-
   return (
     <div className="flex flex-col gap-2 w-1/6 border-r min-h-full p-2">
       <div className="border-small px-1 py-2 rounded-small border-default-200">
         <Listbox
           aria-label="Single selection example"
           variant="flat">
-          {
-            menu.map(item => {
-              <div key={item.id}>
-                <ListboxItem>
-                  <Link href={item.link}>{item.name}</Link>
-                </ListboxItem>
-              </div>
-              return
-            })
-          }
+          <ListboxItem>
+            <Link href="/admin/image">Gallery</Link>
+          </ListboxItem>
+          <ListboxItem>
+            <Link href="/admin/category">Category</Link>
+          </ListboxItem>
+          <ListboxItem>
+            <Link href="/admin/product">Sản phẩm</Link>
+          </ListboxItem>
+          <ListboxItem>
+            <Link href="/admin/blog">Blog</Link>
+          </ListboxItem>
         </Listbox>
       </div>
     </div>

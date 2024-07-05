6a9f4394ca12e0990ecd71d8192b31e81d022b13
TamNguyen1997 <tam070397@gmail.com>
2024-07-05 21:40:59 +0700

    Fix stuff

--- src/app/(customer)/[...slug]/page.js
+++ src/app/(customer)/[...slug]/page.js
@@ -40,9 +40,9 @@ const getPrice = (product) => {
 
   if (product.saleDetails.length === 1) return <>{product.saleDetails[0].price?.toLocaleString()}</>
 
-  if (!product.saleDetails[0].price) return <>{product.saleDetails[product.saleDetails.length - 1].price.toLocaleString()}</>
+  if (!product.saleDetails[0].price) return <>{product.saleDetails[product.saleDetails.length - 1].price?.toLocaleString()}</>
 
-  return <>{product.saleDetails[0].price.toLocaleString()} - {product.saleDetails[product.saleDetails.length - 1].price.toLocaleString()} </>
+  return <>{product.saleDetails[0].price?.toLocaleString()} - {product.saleDetails[product.saleDetails.length - 1].price?.toLocaleString()} </>
 }
 
 const comparePriceDesc = (product1, product2) => {
--- src/app/components/PopularItems.js
+++ src/app/components/PopularItems.js
@@ -53,7 +53,7 @@ const getPrice = (product) => {
 
   if (product.saleDetails.length === 1) return <>{product.saleDetails[0].price?.toLocaleString()}</>
 
-  if (!product.saleDetails[0].price) return <>{product.saleDetails[product.saleDetails.length - 1].price.toLocaleString()}</>
+  if (!product.saleDetails[0].price) return <>{product.saleDetails[product.saleDetails.length - 1].price?.toLocaleString()}</>
 
   return <>{product.saleDetails[0].price?.toLocaleString()} - {product.saleDetails[product.saleDetails.length - 1].price?.toLocaleString()} </>
 }

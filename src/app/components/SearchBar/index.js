55a1513d3abfb24f619607b8dd77a4227b900208
KuronNazaki <huynhhuy.forwork@gmail.com>
2024-07-12 20:58:01 +0700

    feat: integrate function for search bar

--- src/app/components/SearchBar/index.js
+++ src/app/components/SearchBar/index.js
@@ -4,31 +4,59 @@ import { Input } from '@nextui-org/react';
 import { LoaderIcon, Search } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import { useOutsideAlerter } from '@/app/hooks';
 
 const SearchBar = () => {
+  const wrapperRef = useRef(null);
   const [isLoading, setIsLoading] = useState(true);
 
   const [products, setProducts] = useState([]);
-  const [brands, setBrands] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [filteredCategories, setFilteredCategories] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
 
   const [searchText, setSearchText] = useState('');
+
+  useOutsideAlerter(wrapperRef, () => setSearchText(''));
 
   useEffect(() => {
     Promise.all([
       fetch(`/api/products?size=10&page=1`)
         .then((res) => res.json())
         .then((value) => setProducts(value.result)),
-      fetch(`/api/brands`)
+      fetch(`/api/categories`)
         .then((res) => res.json())
-        .then((json) => setBrands(json)),
+        .then((json) => setCategories(json.result)),
     ]).then(() => {
       setIsLoading(false);
     });
   }, []);
+  useEffect(() => {
+    if (searchText) {
+      setFilteredCategories(
+        categories.filter((category) =>
+          category.name.toLowerCase().includes(searchText.toLowerCase())
+        )
+      );
+    } else {
+      setFilteredCategories([]);
+    }
+  }, [categories, searchText]);
+  useEffect(() => {
+    if (filteredCategories.length > 0) {
+      setFilteredProducts(
+        products.filter((product) =>
+          filteredCategories.find((c) => product?.categoryId === c.id)
+        )
+      );
+    } else {
+      setFilteredProducts([]);
+    }
+  }, [filteredCategories, products]);
 
   return (
-    <div className="relative">
+    <div ref={wrapperRef} className="relative">
       <Input
         isClearable
         radius="lg"
@@ -39,6 +67,7 @@ const SearchBar = () => {
         }
         value={searchText}
         onChange={(event) => setSearchText(event.target.value)}
+        onClear={() => setSearchText('')}
       />
       {searchText && (
         <div className="w-[400px] bg-white shadow-lg rounded-lg absolute top-full left-0 mt-2 overflow-hidden z-50">
@@ -46,16 +75,25 @@ const SearchBar = () => {
             <p className="text-slate-600">Có phải bạn đang muốn tìm</p>
           </div>
           {!isLoading ? (
-            <div className="divide-y-small divide-slate-300">
-              {brands.map((brand) => (
-                <div
-                  key={brand.id}
-                  className="px-4 py-2 hover:bg-slate-50 cursor-pointer"
-                >
-                  {brand.name}
-                </div>
-              ))}
-            </div>
+            filteredCategories.length > 0 ? (
+              <div className="divide-y-small divide-slate-300">
+                {filteredCategories.map((category) => (
+                  <Link
+                    key={category.id}
+                    href={`/${category.slug}`}
+                    onClick={() => setSearchText('')}
+                  >
+                    <div className="px-4 py-2 hover:bg-slate-50 cursor-pointer">
+                      {category.name}
+                    </div>
+                  </Link>
+                ))}
+              </div>
+            ) : (
+              <div className="py-2 flex justify-center">
+                Không tìm thấy kết quả
+              </div>
+            )
           ) : (
             <div className="w-full p-2 flex justify-center items-center">
               <LoaderIcon className="animate-spin" />
@@ -65,34 +103,43 @@ const SearchBar = () => {
             <p className="text-slate-600">Sản phẩm gợi ý</p>
           </div>
           {!isLoading ? (
-            <div className="divide-y-small divide-slate-300 py-2">
-              {products.map((product) => (
-                <div key={product.id}>
-                  <Link href={`/san-pham/${product.slug}`}>
-                    <div className="px-4 py-2 flex items-center gap-5 hover:bg-slate-50 cursor-pointer">
-                      <Image
-                        width={60}
-                        height={60}
-                        priority
-                        src={`${
-                          process.env.NEXT_PUBLIC_FILE_PATH +
-                          product.image?.path
-                        }`}
-                        alt={product.image?.name}
-                      />
-                      <div className="">
-                        <h3 className="font-semibold text-slate-600">
-                          {product?.name}
-                        </h3>
-                        <p className="text-slate-400">
-                          {product.category?.name}
-                        </p>
+            filteredProducts.length > 0 ? (
+              <div className="divide-y-small divide-slate-300 py-2">
+                {filteredProducts.map((product) => (
+                  <div key={product.id}>
+                    <Link
+                      href={`/san-pham/${product.id}`}
+                      onClick={() => setSearchText('')}
+                    >
+                      <div className="px-4 py-2 flex items-center gap-5 hover:bg-slate-50 cursor-pointer">
+                        <Image
+                          width={60}
+                          height={60}
+                          priority
+                          src={`${
+                            process.env.NEXT_PUBLIC_FILE_PATH +
+                            product.image?.path
+                          }`}
+                          alt={product.image?.name}
+                        />
+                        <div className="">
+                          <h3 className="font-semibold text-slate-600">
+                            {product?.name}
+                          </h3>
+                          <p className="text-slate-400">
+                            {product.category?.name}
+                          </p>
+                        </div>
                       </div>
-                    </div>
-                  </Link>
-                </div>
-              ))}
-            </div>
+                    </Link>
+                  </div>
+                ))}
+              </div>
+            ) : (
+              <div className="py-2 flex justify-center">
+                Không tìm thấy kết quả
+              </div>
+            )
           ) : (
             <div className="w-full p-2 flex justify-center items-center">
               <LoaderIcon className="animate-spin" />

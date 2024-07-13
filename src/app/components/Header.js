3f419bda37e8641eeaf134eaa00ea9bd1c6c9464
Your Name <hoangtan250997@gmail.com>
2024-07-13 11:54:53 +0700

    [UPDATE] import

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -1,20 +1,21 @@
-"use client"
+"use client";
 
 import { Phone, Search, ShoppingCart } from "lucide-react";
 import Link from "next/link";
 import Image from "next/image";
 import { Button, Input } from "@nextui-org/react";
-import { useEffect, useState } from "react";
-import SearchBar from "@/components/SearchBar"
+import { useEffect, useState, useContext, useRef } from "react";
+import SearchBar from "@/components/SearchBar";
+import { CartContext } from "@/context/CartProvider";
 
 const Header = () => {
-  const [showSubHeader, setShowSubHeader] = useState(false)
+  const [showSubHeader, setShowSubHeader] = useState(false);
 
-  const [subCate, setSubCate] = useState()
-  const [hoveredCate, setHoveredCate] = useState({})
+  const [subCate, setSubCate] = useState();
+  const [hoveredCate, setHoveredCate] = useState({});
   const subCateMenuRef = useRef();
 
-  const { cartdetails } = useContext(CartContext)
+  const { cartdetails } = useContext(CartContext);
 
   return (
     <nav className="bg-black border-gray-200 dark:bg-gray-900 h-[140px]">
@@ -41,18 +42,15 @@ const Header = () => {
                     <span className="pl-1">
                       <ShoppingCart size={24} strokeWidth={2}></ShoppingCart>
                     </span>
-                    <span className="text-sm">
-                      Giỏ hàng
-                    </span>
-                    {
-                      cartdetails?.length ? <div className="rounded-full w-3 h-3 bg-red-600 text-white text-center  text-[10px]">
+                    <span className="text-sm">Giỏ hàng</span>
+                    {cartdetails?.length ? (
+                      <div className="rounded-full w-3 h-3 bg-red-600 text-white text-center  text-[10px]">
                         <span className="animate-ping absolute inline-flex w-3 h-3 rounded-full bg-red-600 opacity-75"></span>
-                        {
-                          cartdetails.length
-                        }
-                      </div> : ""
-                    }
-
+                        {cartdetails.length}
+                      </div>
+                    ) : (
+                      ""
+                    )}
                   </div>
                 </div>
                 <div className="flex items-center text-sm">
@@ -68,22 +66,24 @@ const Header = () => {
                           <div>
                             <Phone></Phone>
                           </div>
-                          <div className="text-lg font-bold">
-                            090 380 2979
-                          </div>
+                          <div className="text-lg font-bold">090 380 2979</div>
                         </div>
                       </Link>
                     </div>
                   </div>
                 </div>
               </div>
-              <div className="h-1/2 flex gap-5 pt-6"
+              <div
+                className="h-1/2 flex gap-5 pt-6"
                 onMouseOver={() => setShowSubHeader(true)}
-                onMouseOut={() => { setShowSubHeader(false) }}>
+                onMouseOut={() => {
+                  setShowSubHeader(false);
+                }}
+              >
                 <HeaderItems
                   onHover={() => setShowSubHeader(true)}
                   onMouseOut={() => {
-                    setShowSubHeader(false)
+                    setShowSubHeader(false);
                   }}
                   setSubCate={setSubCate}
                   setHoveredCate={setHoveredCate}
@@ -93,71 +93,91 @@ const Header = () => {
             </div>
           </div>
         </div>
-        {
-          showSubHeader && subCate?.length ?
-            <div
-              onMouseOver={() => setShowSubHeader(true)}
-              onMouseOut={() => setShowSubHeader(false)}
-              className="z-10 fixed w-full bg-white shadow-lg p-4 subcate-menu" ref={subCateMenuRef}
-            >
-              <div className="container">
-                <h2 className="text-lg font-bold text-black text-left border-b pb-1">{hoveredCate.name || "Thương hiệu"}</h2>
-                <div
-                  className="grid grid-cols-[repeat(auto-fill,minmax(120px,1fr))] gap-3"
-                >
-                  {
-                    subCate.map((subcate, i) =>
-                      <Link
-                        key={i}
-                        href={
-                          hoveredCate.slug ?
-                            `/${hoveredCate.slug}/${subcate.slug}` :
-                            `/${subcate.slug}`
-                        }>
-                        <span className="flex flex-col justify-center items-center hover:opacity-35 hover:shadow-lg py-4" key={subcate.name}>
-                          <div className="flex items-center w-2/3 m-auto h-[80px]">
-                            {
-                              subcate.imageUrl ? <img src={`${process.env.NEXT_PUBLIC_FILE_PATH + subcate.imageUrl}`} width="150" height="150" alt={i} /> : ""
-                            }
-                          </div>
-                          {subcate.name}
-                        </span>
-                      </Link>
-                    )
-                  }
-                </div>
+        {showSubHeader && subCate?.length ? (
+          <div
+            onMouseOver={() => setShowSubHeader(true)}
+            onMouseOut={() => setShowSubHeader(false)}
+            className="z-10 fixed w-full bg-white shadow-lg p-4 subcate-menu"
+            ref={subCateMenuRef}
+          >
+            <div className="container">
+              <h2 className="text-lg font-bold text-black text-left border-b pb-1">
+                {hoveredCate.name || "Thương hiệu"}
+              </h2>
+              <div className="grid grid-cols-[repeat(auto-fill,minmax(120px,1fr))] gap-3">
+                {subCate.map((subcate, i) => (
+                  <Link
+                    key={i}
+                    href={
+                      hoveredCate.slug
+                        ? `/${hoveredCate.slug}/${subcate.slug}`
+                        : `/${subcate.slug}`
+                    }
+                  >
+                    <span
+                      className="flex flex-col justify-center items-center hover:opacity-35 hover:shadow-lg py-4"
+                      key={subcate.name}
+                    >
+                      <div className="flex items-center w-2/3 m-auto h-[80px]">
+                        {subcate.imageUrl ? (
+                          <img
+                            src={`${
+                              process.env.NEXT_PUBLIC_FILE_PATH +
+                              subcate.imageUrl
+                            }`}
+                            width="150"
+                            height="150"
+                            alt={i}
+                          />
+                        ) : (
+                          ""
+                        )}
+                      </div>
+                      {subcate.name}
+                    </span>
+                  </Link>
+                ))}
               </div>
-            </div> : ""
-        }
+            </div>
+          </div>
+        ) : (
+          ""
+        )}
       </div>
     </nav>
-  )
+  );
 };
 
 const BRANDS = [
   {
     slug: "thuong-hieu-mapa",
     name: "Mapa",
-    imageUrl: "/brand/Logo-Mapa.png"
+    imageUrl: "/brand/Logo-Mapa.png",
   },
   {
     slug: "thuong-hieu-rubbermaid",
     name: "Rubbermaid",
-    imageUrl: "/brand/Rubbermaid.png"
+    imageUrl: "/brand/Rubbermaid.png",
   },
   {
     slug: "thuong-hieu-moerman",
     name: "Moerman",
-    imageUrl: "/brand/Logo-Moerman.png"
+    imageUrl: "/brand/Logo-Moerman.png",
   },
   {
     slug: "thuong-hieu-ghibli",
     name: "Ghibli",
-    imageUrl: "/brand/Logo-Ghibli.png"
-  }
-]
+    imageUrl: "/brand/Logo-Ghibli.png",
+  },
+];
 
-const HeaderItems = ({ onHover, onMouseOut, setSubCate, setHoveredCate, subCateMenuRef }) => {
+const HeaderItems = ({
+  onHover,
+  onMouseOut,
+  setSubCate,
+  setHoveredCate,
+  subCateMenuRef,
+}) => {
   // const [windowSize, setWindowSize] = useState({
   //   width: undefined,
   //   height: undefined,
@@ -175,11 +195,13 @@ const HeaderItems = ({ onHover, onMouseOut, setSubCate, setHoveredCate, subCateM
   //   return () => window.removeEventListener("resize", handleResize)
   // }, [])
 
-  const [categories, setCategories] = useState([])
+  const [categories, setCategories] = useState([]);
 
   useEffect(() => {
-    fetch('/api/categories/?includeSubCate=true&size=7').then(res => res.json()).then(json => setCategories(json.result))
-  }, [])
+    fetch("/api/categories/?includeSubCate=true&size=7")
+      .then((res) => res.json())
+      .then((json) => setCategories(json.result));
+  }, []);
 
   // if (windowSize.width <= 1024) {
 
@@ -208,7 +230,7 @@ const HeaderItems = ({ onHover, onMouseOut, setSubCate, setHoveredCate, subCateM
         subCateMenuRef.current.style.top = menuHeight + "px";
         subCateMenuRef.current.style.opacity = 1;
       }
-    }
+    };
 
     handleScroll();
 
@@ -216,38 +238,40 @@ const HeaderItems = ({ onHover, onMouseOut, setSubCate, setHoveredCate, subCateM
     return () => window.removeEventListener("scroll", handleScroll);
   });
 
-  return (<>
-    <div className="w-full" ref={headerItemsRef}>
-      <div className="container flex text-sm items-center text-center">
-        <Link href=""
-          onMouseOver={() => {
-            setSubCate(BRANDS)
-            setHoveredCate({})
-          }}
-          onMouseOut={() => {
-          }}
-          className="p-3"
-        >
-          Thương hiệu
-        </Link>
-        {
-          categories.map((category) =>
-            <Link href={`/${category.slug}`}
+  return (
+    <>
+      <div className="w-full" ref={headerItemsRef}>
+        <div className="container flex text-sm items-center text-center">
+          <Link
+            href=""
+            onMouseOver={() => {
+              setSubCate(BRANDS);
+              setHoveredCate({});
+            }}
+            onMouseOut={() => {}}
+            className="p-3"
+          >
+            Thương hiệu
+          </Link>
+          {categories.map((category) => (
+            <Link
+              href={`/${category.slug}`}
               key={category.id}
               onMouseOver={() => {
-                setSubCate(category.sub_category)
-                onHover()
-                setHoveredCate(category)
+                setSubCate(category.sub_category);
+                onHover();
+                setHoveredCate(category);
               }}
               onMouseOut={onMouseOut}
               className="p-3"
             >
               {category.name}
-            </Link>)
-        }
+            </Link>
+          ))}
+        </div>
       </div>
-    </div>
-  </>)
-}
+    </>
+  );
+};
 
 export default Header;

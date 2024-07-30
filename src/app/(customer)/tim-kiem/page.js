73b449030d227a5dfd9bd2f0d37416d61f7f9e6e
TamNguyen1997 <tam070397@gmail.com>
2024-07-30 22:59:10 +0700

    Update search

--- src/app/(customer)/tim-kiem/page.js
+++ src/app/(customer)/tim-kiem/page.js
@@ -3,12 +3,13 @@
 import { useSearchParams } from "next/navigation";
 import { useEffect, useMemo, useState } from "react";
 import { getTotalPages } from "@/lib/pagination"
-import { Link, Pagination } from "@nextui-org/react";
-import { getPrice } from "@/lib/product";
+import { Pagination } from "@nextui-org/react";
+import { Suspense } from 'react'
+import ProductCard from "@/components/product/ProductCard"
 
 const rowsPerPage = 20
 
-const Search = () => {
+const SearchBar = () => {
   const [data, setData] = useState([])
   const searchParams = useSearchParams()
   const [total, setTotal] = useState(0)
@@ -36,39 +37,16 @@ const Search = () => {
   return (
     <>
       <link rel="canonical" href={`${process.env.NEXT_PUBLIC_DOMAIN}/tim-kiem`} />
-      {/* <div
-        className="flex flex-col items-center 
-        bg-[image:var(--image-url)] bg-no-repeat bg-center bg-cover
-        justify-center min-w-screen h-72 md:h-52 sm:h-32"
-        style={{
-          '--image-url': `url(${category.image ? process.env.NEXT_PUBLIC_FILE_PATH + category.image.path : ""})`,
-          backgroundSize: "100% 100%"
-        }} >
-      </div> */}
+
       <div className="w-9/12 mx-auto pt-5">
         <p className="font-bold p-2">
           Tìm thấy {total} kết quả.
         </p>
         <div className="w-full my-5 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 p-2">
           {data.map((product) => (
-            <Link
-              href={`/ ${product.subCate.slug} / ${product.slug}`}
-              key={product.id}
-              className="group border rounded overflow-clip">
-              <img
-                src={`${process.env.NEXT_PUBLIC_FILE_PATH + product?.image?.path}`}
-                alt={product?.imageAlt}
-                className="object-cover object-center group-hover:opacity-50 p-2 hover:-translate-y-2.5 hover:scale-[1.02] hover:shadow-[0px_10px_10px_rgba(0,0,0,0.15)]" />
-              <p className="mt-4 text-sm text-gray-700 font-semibold text-center">
-                {product.name}
-              </p>
-
-              <p className="text-center text-red-500 font-bold text-xl pt-3 border-t-medium">
-                {
-                  getPrice(product)
-                }
-              </p>
-            </Link>
+            <div key={product.id} className="h-full p-2 hover:opacity-75">
+              <ProductCard product={product} />
+            </div>
           ))}
         </div>
         <div className="flex w-full justify-center">
@@ -82,9 +60,19 @@ const Search = () => {
           />
         </div>
       </div>
+
     </>
   );
 };
 
+function Search() {
+  return <Suspense fallback={<SearchBarFallback />}>
+    <SearchBar />
+  </Suspense>
+}
+
+function SearchBarFallback() {
+  return <></>
+}
 
 export default Search;

17aaa73e48e2cfb7fd7a82b24ccbdee988c3e2f7
TamNguyen1997 <tam070397@gmail.com>
2024-07-30 23:19:30 +0700

    Fix card

--- src/app/components/product/ProductCard.js
+++ src/app/components/product/ProductCard.js
@@ -7,17 +7,15 @@ const ProductCard = ({ product }) => {
       <div className="rounded-md border object-cover object-center group-hover:opacity-50
             hover:-translate-y-2.5 hover:scale-[1.02] shadow-[0px_2px_10px_rgba(0,0,0,0.15)]
             hover:shadow-[0px_10px_10px_rgba(0,0,0,0.15)] overflow-hidden transition">
-        <Link href={`/${product.subCate.slug}/${product.slug}`} className="flex flex-col h-full">
-          <div className="h-2/3">
-            <div className="aspect-h-1 aspect-w-1 w-full h-full overflow-hidden xl:aspect-h-8 xl:aspect-w-7">
-              <img
-                width={500}
-                height={400}
-                src={`${process.env.NEXT_PUBLIC_FILE_PATH + product.image?.path}`}
-                alt={product.imageAlt}
-                className="h-full w-full object-cover object-center group-hover:opacity-75 hover:scale-110 transition"
-              />
-            </div>
+        <Link href={`/${product.subCate.slug}/${product.slug}`} className="flex flex-col h-[300px]">
+          <div className="aspect-h-1 aspect-w-1 w-full h-full overflow-hidden xl:aspect-h-8 xl:aspect-w-7">
+            <img
+              width={300}
+              height={300}
+              src={`${product.image?.path}`}
+              alt={product.imageAlt}
+              className="h-full w-full object-cover object-center group-hover:opacity-75 hover:scale-110 transition"
+            />
           </div>
         </Link>
       </div>

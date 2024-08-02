32a29053ec295077d1c218f00249ee0891c8cd7c
TamNguyen1997 <tam070397@gmail.com>
2024-08-02 17:21:46 +0700

    Update stuff

--- src/app/components/product/ProductCard.js
+++ src/app/components/product/ProductCard.js
@@ -27,7 +27,7 @@ const ProductCard = ({ product }) => {
         </div>
       </Link>
       {
-        getPrice(product) ? <p className="text-red-500 font-bold">{getPrice(product).toLocaleString()} đ</p> : ""
+        getPrice(product) ? <p className="text-red-500 font-bold">{getPrice(product)} đ</p> : ""
       }
     </div>
   )
--- src/lib/product/index.js
+++ src/lib/product/index.js
@@ -1,9 +1,9 @@
 const getPrice = (product) => {
   if (!product.saleDetails?.length) return null
 
-  if (product.saleDetails.length === 1) return product.saleDetails[0].price
+  if (product.saleDetails.length === 1) return product.saleDetails[0].price.toLocaleString()
 
-  if (!product.saleDetails[0].price) return product.saleDetails[product.saleDetails.length - 1].price
+  if (!product.saleDetails[0].price) return product.saleDetails[product.saleDetails.length - 1].price.toLocaleString()
 
   return <>{product.saleDetails[0].price?.toLocaleString()} - {product.saleDetails[product.saleDetails.length - 1].price?.toLocaleString()} </>
 }

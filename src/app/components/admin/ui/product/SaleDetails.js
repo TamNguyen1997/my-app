6b16949ccab9bd9fd117b40521a73aba8f8f6081
TamNguyen1997 <tam070397@gmail.com>
2024-09-11 21:14:51 +0700

    Update stuff

--- src/app/components/admin/ui/product/SaleDetails.js
+++ src/app/components/admin/ui/product/SaleDetails.js
@@ -1,4 +1,4 @@
-import { Button, Input, Select, SelectItem } from "@nextui-org/react"
+import { Button, Checkbox, Input, Select, SelectItem } from "@nextui-org/react"
 import { Trash2 } from "lucide-react"
 import { useEffect, useState } from "react"
 import { toast, ToastContainer } from "react-toastify"
@@ -54,7 +54,7 @@ const ColorSelect = ({ detail, setDetails, details }) => {
 }
 
 const removeItem = (id, details) => {
-  return details.filter(item => item.id != id)
+  return details.filter(item => item.id != id).filter(item => item.saleDetailId !== id)
 }
 
 const handleDetailChange = (saleDetails, id, value) => {
@@ -108,6 +108,27 @@ const SecondarySaleDetails = ({ saleDetails, setSaleDetails, saleDetail }) => {
                     max={999999999}
                     onValueChange={(value) => setSaleDetails(handleDetailChange(saleDetails, detail.id, { price: parseInt(value) }))}
                   />
+                  <Input type="number"
+                    label="Giá giảm"
+                    defaultValue={detail.promotionalPrice}
+                    aria-label="Giá"
+                    min={0}
+                    max={999999999}
+                    onValueChange={(value) => setSaleDetails(handleDetailChange(saleDetails, detail.id, { promotionalPrice: parseInt(value) }))}
+                  />
+                  <Input type="number"
+                    label="Tồn kho"
+                    defaultValue={detail.inStock}
+                    aria-label="Giá"
+                    min={0}
+                    max={999999999}
+                    onValueChange={(value) => setSaleDetails(handleDetailChange(saleDetails, detail.id, { inStock: parseInt(value) }))}
+                  />
+                  <Checkbox className="w-full"
+                    isSelected={detail.showPrice}
+                    onValueChange={value => setSaleDetails(handleDetailChange(saleDetails, detail.id, { showPrice: value }))}>
+                    Hiện giá
+                  </Checkbox>
                 </div>
               </div>
 
@@ -127,9 +148,11 @@ const SecondarySaleDetails = ({ saleDetails, setSaleDetails, saleDetail }) => {
 
 const SaleDetails = ({ product }) => {
   const [saleDetails, setSaleDetails] = useState([])
+  const [filters, setFilters] = useState([])
 
   useEffect(() => {
     fetch(`/api/products/${product.id}/sale-details`).then(res => res.json()).then(setSaleDetails)
+    fetch(`/api/filters/`).then(res => res.json()).then(json => setFilters(json.result))
   }, [product])
 
   const addEmptySaleDetail = () => {
@@ -160,7 +183,7 @@ const SaleDetails = ({ product }) => {
           saleDetails.filter(item => !item.saleDetailId).map(detail => {
             return <div key={detail.id}>
               <div className="flex" >
-                <div className="w-11/12">
+                <div className="w-full">
                   <div className="flex gap-2">
                     <Input
                       type="text"
@@ -169,30 +192,20 @@ const SaleDetails = ({ product }) => {
                       aria-label="SKU"
                       onValueChange={value => setSaleDetails(handleDetailChange(saleDetails, detail.id, { sku: value }))}
                     />
-                    <Select
-                      label="Loại"
-                      defaultSelectedKeys={new Set([detail.type])}
-                      onSelectionChange={(value) => setSaleDetails(handleDetailChange(saleDetails, detail.id, { type: value.values().next().value }))}
-                    >
-                      <SelectItem key="COLOR">
-                        Màu
-                      </SelectItem>
-                      <SelectItem key="TEXT">
-                        Text
-                      </SelectItem>
+                    <Select label="Filter"
+                      defaultSelectedKeys={[detail.filterId]}
+                      onSelectionChange={(value) => setSaleDetails(handleDetailChange(saleDetails, detail.id, { filterId: value.values().next().value }))}>
+                      {
+                        filters.map(item => <SelectItem key={item.id}>{item.name}</SelectItem>)
+                      }
                     </Select>
-                    {
-                      detail.type === "TEXT" ?
-                        <Input
-                          type="text"
-                          label="Option"
-                          defaultValue={detail.value}
-                          aria-label={detail.value}
-                          onValueChange={value => setSaleDetails(handleDetailChange(saleDetails, detail.id, { value: value }))}
-                        />
-                        : <ColorSelect detail={detail} setDetails={setSaleDetails} details={saleDetails} />
-                    }
 
+                    <FilterValueSelect
+                      detail={detail} getFilter={() =>
+                        filters.find(filter => filter.id === saleDetails.find(sale => detail.id === sale.id).filterId)
+                      }
+                      setDetail={setSaleDetails}
+                      onSelectionChange={handleDetailChange} details={saleDetails} />
                     <Input type="number"
                       label="Giá"
                       defaultValue={detail.price}
@@ -201,19 +214,37 @@ const SaleDetails = ({ product }) => {
                       max={999999999}
                       onValueChange={(value) => setSaleDetails(handleDetailChange(saleDetails, detail.id, { price: parseInt(value) }))}
                     />
+                    <Input type="number"
+                      label="Giá giảm"
+                      defaultValue={detail.promotionalPrice}
+                      aria-label="Giá"
+                      min={0}
+                      max={999999999}
+                      onValueChange={(value) => setSaleDetails(handleDetailChange(saleDetails, detail.id, { promotionalPrice: parseInt(value) }))}
+                    />
+                    <Input type="number"
+                      label="Tồn kho"
+                      defaultValue={detail.inStock}
+                      aria-label="Giá"
+                      min={0}
+                      max={999999999}
+                      onValueChange={(value) => setSaleDetails(handleDetailChange(saleDetails, detail.id, { inStock: parseInt(value) }))}
+                    />
+                    <Checkbox isSelected={detail.showPrice}
+                      className="w-full"
+                      onValueChange={value => setSaleDetails(handleDetailChange(saleDetails, detail.id, { showPrice: value }))}>
+                      Hiện giá
+                    </Checkbox>
                     <div className="flex text-center items-center">
                       <Button onClick={() => {
                         setSaleDetails([...saleDetails, { id: v4(), type: "TEXT", saleDetailId: detail.id, productId: product.id }])
                       }}>
-                        Thêm chi tiết
+                        Thêm
                       </Button>
+                      <div className="text-lg text-danger cursor-pointer active:opacity-50 pl-5 float-right">
+                        <Trash2 onClick={() => setSaleDetails(removeItem(detail.id, saleDetails))} />
+                      </div>
                     </div>
-                  </div>
-                </div>
-
-                <div className="pt-3 pl-3">
-                  <div className="text-lg text-danger cursor-pointer active:opacity-50 pl-5 float-right">
-                    <Trash2 onClick={() => removeDetail(detail.id)} />
                   </div>
                 </div>
               </div>
@@ -231,6 +262,21 @@ const SaleDetails = ({ product }) => {
         </div>
       </div>
     </>
+  )
+}
+
+const FilterValueSelect = ({ detail, getFilter, setDetail, onSelectionChange, details }) => {
+
+  const filterValues = getFilter()?.filterValue || []
+
+  return (
+    <Select label="Giá trị filter" isDisabled={!filterValues.length}
+      defaultSelectedKeys={[detail.filterValueId]}
+      onSelectionChange={value => setDetail(onSelectionChange(details, detail.id, { filterValueId: value.values().next().value }, detail.id))}>
+      {
+        filterValues.map(item => <SelectItem key={item.id}>{item.value}</SelectItem>)
+      }
+    </Select>
   )
 }
 

b7ac2ba626a3336ad34feb2aecbf70d246dd557e
TamNguyen1997 <tam070397@gmail.com>
2024-09-27 00:07:39 +0700

    Update stuff

--- src/lib/product/index.js
+++ src/lib/product/index.js
@@ -1,11 +1,13 @@
 const getPrice = (product) => {
   if (!product.saleDetails?.length) return null
 
-  if (product.saleDetails.length === 1) return product.saleDetails[0].price.toLocaleString()
+  if (product.saleDetails.length === 1) return product.saleDetails[0].price.toLocaleString()?.replace(",", ".")
 
-  if (!product.saleDetails[0].price) return product.saleDetails[product.saleDetails.length - 1].price.toLocaleString()
+  if (!product.saleDetails[0].price) return product.saleDetails[product.saleDetails.length - 1].price.toLocaleString()?.replace(",", ".")
 
-  return <>{product.saleDetails[0].price?.toLocaleString()} - {product.saleDetails[product.saleDetails.length - 1].price?.toLocaleString()} </>
+  return <>{product.saleDetails[0].price?.toLocaleString()?.replace(",", ".")}
+    -
+    {product.saleDetails[product.saleDetails.length - 1].price?.toLocaleString()?.replace(",", ".")} </>
 }
 
 export { getPrice }

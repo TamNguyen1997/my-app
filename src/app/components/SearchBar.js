76f0f462e308358a054b4fa80371de5bed602ab4
TamNguyen1997 <tam070397@gmail.com>
2025-01-13 22:15:07 +0700

    Update stuff

--- src/app/components/SearchBar.js
+++ src/app/components/SearchBar.js
@@ -27,7 +27,7 @@ const SearchBar = () => {
       (key) =>
         filteredCondition[key] === undefined && delete filteredCondition[key]
     );
-    const queryString = new URLSearchParams({ slug: slugify(filteredCondition.slug) }).toString();
+    const queryString = new URLSearchParams({ slug: slugify(filteredCondition.slug || "") }).toString();
 
     fetch(`/api/categories/?size=${5}&page=${1}&${queryString}`).then(
       async (res) => {

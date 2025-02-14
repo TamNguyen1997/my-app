ac2dc0b16f21caee8913e7e61d4c1fd5715e0b6a
TamNguyen1997 <tam070397@gmail.com>
2025-02-14 11:22:24 +0700

    Update stuff

--- src/app/(customer)/kien-thuc-hay/[_id]/page.js
+++ src/app/(customer)/kien-thuc-hay/[_id]/page.js
@@ -1,6 +1,19 @@
 import { BlogDetail } from "@/components/blog/BlogDetail"
 
-const Information = ({ params }) => {
+export async function generateMetadata({ params }) {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_WORDPRESS_URL}/wp-json/wp/v2/posts/?slug=${params._id}&_embed&categories_exclude=${process.env.NEXT_PUBLIC_WORDPRESS_PRODUCT_CATEGORY_ID}`)
+  if (!res.ok) {
+    return {}
+  }
+
+  const blog = (await res.json())[0]
+  return {
+    title: blog?.yoast_head_json?.og_title,
+    description: blog?.yoast_head_json?.og_description,
+  }
+}
+
+const Information = async ({ params }) => {
   return (
     <>
       <link rel="canonical" href={`${process.env.NEXT_PUBLIC_DOMAIN}/kien-thuc-hay/${params._id}`} />

c47161a1a35a1cff49d9e1fb165c090d32e570f6
TerriNguyen <tam070397@gmail.com>
2025-03-08 13:24:14 +0700

    update stuff

--- src/app/(customer)/tin-tuc/[_id]/page.js
+++ src/app/(customer)/tin-tuc/[_id]/page.js
@@ -1,7 +1,7 @@
 import { BlogDetail } from "@/components/blog/BlogDetail"
 
 export async function generateMetadata({ params }) {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_WORDPRESS_URL}/wp-json/wp/v2/posts/?slug=${params._id}&_embed&categories_exclude=${process.env.NEXT_PUBLIC_WORDPRESS_PRODUCT_CATEGORY_ID}`)
+  const res = await fetch(`${process.env.NEXT_PUBLIC_WORDPRESS_URL}/wp-json/wp/v2/posts/?slug=${params._id}&_embed`)
   if (!res.ok) {
     return {}
   }

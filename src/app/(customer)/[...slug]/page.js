a12126c3887d4d160b35a011562dd3474f8c76c5
TamNguyen1997 <tam070397@gmail.com>
2025-03-12 17:32:04 +0700

    update

--- src/app/(customer)/[...slug]/page.js
+++ src/app/(customer)/[...slug]/page.js
@@ -38,57 +38,55 @@ const Page = async ({ params }) => {
     }
     return <Category category={category} productFilter={filter} />
   }
-  if (categorySlug && productSlug) {
-    const product = await db.product.findFirst({
-      where: {
-        AND: [
-          {
-            OR: [
-              { brand: { slug: categorySlug } },
-              { category: { slug: categorySlug } },
-              { subCate: { slug: categorySlug } }
-            ]
-          },
-          {
-            slug: productSlug
-          }
-        ]
-      }, include: {
-        technical_detail: {
-          include: {
-            filterValue: true,
-            filter: true
-          }
+  const product = await db.product.findFirst({
+    where: {
+      AND: [
+        {
+          OR: [
+            { brand: { slug: categorySlug } },
+            { category: { slug: categorySlug } },
+            { subCate: { slug: categorySlug } }
+          ]
         },
-        saleDetails: {
-          include: {
-            filter: true,
-            filterValue: true
-          }
+        {
+          slug: productSlug
+        }
+      ]
+    }, include: {
+      technical_detail: {
+        include: {
+          filterValue: true,
+          filter: true
+        }
+      },
+      saleDetails: {
+        include: {
+          filter: true,
+          filterValue: true
+        }
+      },
+      image: true,
+      category: true,
+      subCate: true,
+      product_on_image: {
+        orderBy: {
+          order: 'asc'
         },
-        image: true,
-        category: true,
-        subCate: true,
-        product_on_image: {
-          orderBy: {
-            order: 'asc'
-          },
-          include: { image: true }
-        },
-        brand: true
-      }
-    })
-    if (product) {
-      let description = "";
-      const productPostResponse = await fetch(`${process.env.NEXT_PUBLIC_WORDPRESS_URL}/wp-json/wp/v2/posts/?slug=${product.slug}&categories=${process.env.NEXT_PUBLIC_WORDPRESS_PRODUCT_CATEGORY_ID}`);
-      if (productPostResponse.ok) {
-        const productPost = await productPostResponse.json();
-        description = productPost[0]?.content?.rendered;
-      }
-      return <ProductDetail id={params.slug[1]} product={product} description={description} />
+        include: { image: true }
+      },
+      brand: true
     }
+  })
+  if (product) {
+    let description = "";
+    const productPostResponse = await fetch(`${process.env.NEXT_PUBLIC_WORDPRESS_URL}/wp-json/wp/v2/posts/?slug=${product.slug}&categories=${process.env.NEXT_PUBLIC_WORDPRESS_PRODUCT_CATEGORY_ID}`);
+    if (productPostResponse.ok) {
+      const productPost = await productPostResponse.json();
+      description = productPost[0]?.content?.rendered;
+    }
+    return <ProductDetail id={params.slug[1]} product={product} description={description} />
   }
-  return notFound()
+
 }
 
 export default Page;

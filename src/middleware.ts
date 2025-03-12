8fe0b4eb3a7fcc0b37c581ba733a7b28c0526dac
TerriNguyen <tam070397@gmail.com>
2025-03-12 22:51:24 +0700

    update stuff

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,8 @@ import { RedirectHandler } from './middlewares/RedirectHandler'
 import { LoginHandler } from './middlewares/LoginHandler'
 import { AdminAuthentication } from './middlewares/AdminAuthentication'
 import { AdminAuthorization } from './middlewares/AdminAuthorization'
+import { RemoveParenthesisRedirectHandler } from './middlewares/RemoveParenthesisRedirectHandler'
 
-const middlewares = [LoginHandler, AdminAuthentication, AdminAuthorization, RedirectHandler];
+const middlewares = [LoginHandler, AdminAuthentication, AdminAuthorization, RedirectHandler, RemoveParenthesisRedirectHandler];
 
 export default stackMiddlewares(middlewares)

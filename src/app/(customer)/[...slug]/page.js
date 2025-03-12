dce2b311c641c4bf7ec76b20b67d7b9be4b71b43
TamNguyen1997 <tam070397@gmail.com>
2025-03-12 17:53:52 +0700

    update stuff

--- src/app/(customer)/[...slug]/page.js
+++ src/app/(customer)/[...slug]/page.js
@@ -4,7 +4,6 @@ import Category from "@/app/components/product/Category";
 import CategoryNotFound from "@/app/components/CategoryNotFound";
 import { db } from '@/app/db';
 import { cate_type } from "@prisma/client";
-import { notFound } from "next/navigation";
 
 export async function generateMetadata({ params }) {
   if (params.slug.length === 1) {
@@ -23,11 +22,8 @@ export async function generateMetadata({ params }) {
 }
 
 const Page = async ({ params }) => {
-  const categorySlug = Array.isArray(params.slug) ? params.slug[0] : null;
-  const productSlug = Array.isArray(params.slug) ? params.slug[1] : null;
-
-  if (categorySlug && !productSlug) {
-    const [slug, filter] = categorySlug.split("#")
+  if (params.slug.length === 1) {
+    const [slug, filter] = params.slug[0].split("#")
     const category = await db.category.findFirst({ where: { slug: slug }, include: { subcates: true, image: true } })
 
     if (!category) {
@@ -38,57 +34,7 @@ const Page = async ({ params }) => {
     }
     return <Category category={category} productFilter={filter} />
   }
-  if (categorySlug && productSlug) {
-    const product = await db.product.findFirst({
-      where: {
-        AND: [
-          {
-            OR: [
-              { brand: { slug: categorySlug } },
-              { category: { slug: categorySlug } },
-              { subCate: { slug: categorySlug } }
-            ]
-          },
-          {
-            slug: productSlug
-          }
-        ]
-      }, include: {
-        technical_detail: {
-          include: {
-            filterValue: true,
-            filter: true
-          }
-        },
-        saleDetails: {
-          include: {
-            filter: true,
-            filterValue: true
-          }
-        },
-        image: true,
-        category: true,
-        subCate: true,
-        product_on_image: {
-          orderBy: {
-            order: 'asc'
-          },
-          include: { image: true }
-        },
-        brand: true
-      }
-    })
-    if (product) {
-      let description = "";
-      const productPostResponse = await fetch(`${process.env.NEXT_PUBLIC_WORDPRESS_URL}/wp-json/wp/v2/posts/?slug=${product.slug}&categories=${process.env.NEXT_PUBLIC_WORDPRESS_PRODUCT_CATEGORY_ID}`);
-      if (productPostResponse.ok) {
-        const productPost = await productPostResponse.json();
-        description = productPost[0]?.content?.rendered;
-      }
-      return <ProductDetail id={params.slug[1]} product={product} description={description} />
-    }
-  }
-  return notFound()
+  return <ProductDetail id={params.slug[1]} />
 }
 
 export default Page;

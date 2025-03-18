38e3c64db71e5d1570b721f0fac5e6db8c73978b
TamNguyen1997 <tam070397@gmail.com>
2025-03-18 23:09:48 +0700

    Update stuff

--- src/app/components/product/ProductDetail.js
+++ src/app/components/product/ProductDetail.js
@@ -7,8 +7,13 @@ import ProductImageCarousel from "@/components/ProductImageCarousel";
 import ProductDetailTabs from "@/components/ProductDetailTabs";
 import { motion } from "framer-motion";
 import { addRecentlyView } from "@/lib/product";
+import { addRecentlyView } from "@/lib/product";
 
-export default ({ product = {}, description = "", relatedProducts = [] }) => {
+export default ({ id }) => {
+  const [product, setProduct] = useState({});
+  const [images, setImages] = useState([]);
+  const [description, setDescription] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
     addRecentlyView(product)
   }, [product.id])

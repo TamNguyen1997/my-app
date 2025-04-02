a45c644a8f0493f2e6f8f5416b87fd7839099982
TamNguyen1997 <tam070397@gmail.com>
2025-04-02 14:35:36 +0700

    refactor stuff

--- src/app/(customer)/layout.js
+++ src/app/(customer)/layout.js
@@ -6,14 +6,14 @@ import CartProvider from '@/context/CartProvider'
 import { db } from '@/app/db'
 
 export default async function RootLayout({ children }) {
-  const headers = await getHeaders()
+  const headers = getHeaders()
   return (
     <html lang="vi">
       <meta name="robots" content="index,follow" />
       <meta name="google-site-verification" content="bnj-0vSnMlKuLmNzj5kleHIQ2Sk85O1ZqwB4-fGPIHo" />
       <body className='font-roboto'>
         <CartProvider>
-          <Header headers={headers} />
+          <Header headers={await headers} />
           <div className="min-h-screen">
             {children}
           </div>

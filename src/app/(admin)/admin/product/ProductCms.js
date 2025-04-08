f76c6d336a8562b1750d5f84f82a5e90eed236c7
TerriNguyen <terri.nguyen@thoughtworks.com>
2025-04-08 16:30:08 +0700

    Refactor product CMS

--- src/app/(admin)/admin/product/ProductCms.js
+++ src/app/(admin)/admin/product/ProductCms.js
@@ -33,7 +33,6 @@ const quickUpdateProduct = async (product, value) => {
 
 const ProductCms = ({ categories = [] }) => {
   const [loadingState, setLoadingState] = useState("loading")
-
   const [selectedCate, setSelectedCate] = useState({})
   const [condition, setCondition] = useState({})
   const [total, setTotal] = useState(0)
@@ -47,11 +46,7 @@ const ProductCms = ({ categories = [] }) => {
   const deleteConfirmationDisclosure = useDisclosure()
   const deleteManyConfirmationDisclosure = useDisclosure()
 
-  useEffect(() => {
-    getProduct()
-  }, [page, rowsPerPage])
-
-  const getProduct = async () => {
+  const fetchProducts = useCallback(async () => {
     setLoadingState("loading")
     let filteredCondition = { ...condition }
     Object.keys(filteredCondition).forEach(
@@ -60,51 +55,64 @@ const ProductCms = ({ categories = [] }) => {
     )
     const queryString = new URLSearchParams(filteredCondition).toString()
 
-    await fetch(
-      `/api/products/v2?size=${rowsPerPage}&page=${page}&${queryString}`
-    ).then(async (res) => {
+    try {
+      const res = await fetch(
+        `/api/products/v2?size=${rowsPerPage}&page=${page}&${queryString}`
+      )
       const data = await res.json()
       setProducts(data.result)
       setTotal(data.total)
       setLoadingState("idle")
-    })
-  }
+    } catch (error) {
+      setLoadingState("idle")
+      toast.error("An error occurred while fetching the products.")
+    }
+  }, [page, rowsPerPage, condition])
+
+  useEffect(() => {
+    fetchProducts()
+  }, [fetchProducts])
 
   const pages = useMemo(() => {
     return total ? Math.ceil(total / rowsPerPage) : 0
   }, [total, rowsPerPage])
 
   const deleteMany = async () => {
-    const productsToDelete = selectedKeys === 'all' ? products.map(item => item.id) : [...selectedKeys]
-
-    await Promise
-      .all(productsToDelete.map(id => fetch(`/api/products/${id}`, { method: "DELETE" })))
-      .then(response => {
-        response.forEach(async res => {
-          if (res.ok) {
-            toast.success(`Đã xóa sản phẩm`)
-          } else {
-            const body = await res.json()
-            toast.error(`${body.message}`)
-          }
-        })
+    const productsToDelete = selectedKeys === "all" ? products.map(item => item.id) : [...selectedKeys]
+    try {
+      const responses = await Promise.all(
+        productsToDelete.map(id => fetch(`/api/products/${id}`, { method: "DELETE" }))
+      )
+      responses.forEach(async (res) => {
+        if (res.ok) {
+          toast.success("Deleted product successfully")
+        } else {
+          const body = await res.json()
+          toast.error(body.message)
+        }
       })
-
-    setSelectedKeys([])
-    deleteManyConfirmationDisclosure.onClose()
-    getProduct()
+      setSelectedKeys([])
+      deleteManyConfirmationDisclosure.onClose()
+      fetchProducts()
+    } catch (error) {
+      toast.error("An error occurred while deleting products.")
+    }
   }
 
   const deleteProduct = async () => {
     if (!productIdToDelete) return
-    const res = await fetch(`/api/products/${productIdToDelete}`, { method: "DELETE" })
-    if (res.ok) {
-      getProduct()
-      deleteConfirmationDisclosure.onClose()
-      toast.success("Đã xóa sản phẩm")
-    } else {
-      const body = await res.json()
-      toast.error(body.message)
+    try {
+      const res = await fetch(`/api/products/${productIdToDelete}`, { method: "DELETE" })
+      if (res.ok) {
+        toast.success("Deleted product successfully")
+        fetchProducts()
+        deleteConfirmationDisclosure.onClose()
+      } else {
+        const body = await res.json()
+        toast.error(body.message)
+      }
+    } catch (error) {
+      toast.error("An error occurred while deleting the product.")
     }
     setProductIdToDelete()
   }
@@ -115,59 +123,52 @@ const ProductCms = ({ categories = [] }) => {
       case "actions":
         return (
           <div className="relative flex items-center gap-2">
-            <span className="text-lg text-default-400 cursor-pointer active:opacity-50 pt-2">
-              <Link href={`/admin/product/edit/${product.id}`}>
-                <EditIcon />
-              </Link>
-            </span>
-            <span className="text-lg text-danger cursor-pointer active:opacity-50 pl-2">
-              <Trash2 onClick={() => {
+            <Link href={`/admin/product/edit/${product.id}`} className="text-lg text-default-400 cursor-pointer">
+              <EditIcon />
+            </Link>
+            <span
+              className="text-lg text-danger cursor-pointer"
+              onClick={() => {
                 deleteConfirmationDisclosure.onOpen()
                 setProductIdToDelete(product.id)
-              }} />
+              }}
+            >
+              <Trash2 />
             </span>
-            <span className="text-lg cursor-pointer text-green-400">
-              {(product.imageUrl) && <Tooltip showArrow content="Có thumbnail">
-                <FileImage />
-              </Tooltip>}
-            </span>
+            {product.imageUrl && (
+              <Tooltip showArrow content="Có thumbnail" >
+                <span className="text-lg cursor-pointer text-green-400">
+                  <FileImage />
+                </span>
+              </Tooltip>
+            )}
           </div>
         )
       case "active":
-        return (
-          <div className="relative flex items-center gap-2">
-            <Switch
-              defaultSelected={product.active}
-              onValueChange={(value) =>
-                quickUpdateProduct(product, { active: value })
-              }
-            ></Switch>
-          </div>
-        )
       case "highlight":
         return (
-          <div className="relative flex items-center gap-2">
-            <Switch
-              defaultSelected={product.highlight}
-              onValueChange={(value) =>
-                quickUpdateProduct(product, { highlight: value })
-              }
-            ></Switch>
-          </div>
+          <Switch
+            defaultSelected={product[columnKey]}
+            onValueChange={(value) => quickUpdateProduct(product, { [columnKey]: value })}
+          />
         )
       case "cate":
         return product.category?.name
       case "subcate":
         return product.subCate?.name
       case "id":
-        return (<Snippet color="default" symbol="" className="!font-open_san !bg-white"><p className="!font-open_san">{cellValue}</p></Snippet>)
+        return (
+          <Snippet color="default" symbol="" className="!font-open_san !bg-white">
+            <p className="!font-open_san">{cellValue}</p>
+          </Snippet>
+        )
       default:
         return cellValue
     }
   }, [])
 
   const onConditionChange = (value) => {
-    setCondition(Object.assign({}, condition, value))
+    setCondition((prevCondition) => ({ ...prevCondition, ...value }))
   }
 
   return (
@@ -184,16 +185,13 @@ const ProductCms = ({ categories = [] }) => {
             value={condition.id_name}
             onValueChange={(value) => {
               onConditionChange({ id_name: value })
-              if (value.length > 2) getProduct()
+              if (value.length > 2) fetchProducts()
             }}
           />
-
           <Select
             label="Nổi bật"
             labelPlacement="outside"
-            onSelectionChange={(value) =>
-              onConditionChange({ highlight: value.values().next().value })
-            }
+            onSelectionChange={(value) => onConditionChange({ highlight: value.values().next().value })}
           >
             <SelectItem key="true">Nổi bật</SelectItem>
             <SelectItem key="false">Không nổi bật</SelectItem>
@@ -201,9 +199,7 @@ const ProductCms = ({ categories = [] }) => {
           <Select
             label="Active"
             labelPlacement="outside"
-            onSelectionChange={(value) =>
-              onConditionChange({ active: value.values().next().value })
-            }
+            onSelectionChange={(value) => onConditionChange({ active: value.values().next().value })}
           >
             <SelectItem key="true">Active</SelectItem>
             <SelectItem key="false">Inactive</SelectItem>
@@ -211,49 +207,46 @@ const ProductCms = ({ categories = [] }) => {
           <Select
             label="Thumbnail"
             labelPlacement="outside"
-            onSelectionChange={(value) =>
-              onConditionChange({ thumbnail: value.values().next().value })
-            }
+            onSelectionChange={(value) => onConditionChange({ thumbnail: value.values().next().value })}
           >
             <SelectItem key="true">Có thumbnail</SelectItem>
             <SelectItem key="false">Không có thumbnail</SelectItem>
           </Select>
-
           <Select
             label="Category"
             labelPlacement="outside"
             onSelectionChange={(value) => {
               const categoryId = value.values().next().value
-              onConditionChange({ categoryId: categoryId })
+              onConditionChange({ categoryId })
               setSelectedCate(categories.find(item => item.id === categoryId))
-            }}>
-            {
-              categories.map((category) => (<SelectItem key={category.id}>{category.name}</SelectItem>))
-            }
+            }}
+          >
+            {categories.map((category) => (
+              <SelectItem key={category.id}>{category.name}</SelectItem>
+            ))}
           </Select>
           <Select
             isDisabled={!selectedCate?.subcates?.length}
             label="Subcategory"
             labelPlacement="outside"
-            onSelectionChange={(value) => {
-              onConditionChange({ subCateId: value.values().next().value })
-            }}>
-            {
-              selectedCate?.subcates?.map((subCate) => (<SelectItem key={subCate.id}>{subCate.name}</SelectItem>))
-            }
+            onSelectionChange={(value) => onConditionChange({ subCateId: value.values().next().value })}
+          >
+            {selectedCate?.subcates?.map((subCate) => (
+              <SelectItem key={subCate.id}>{subCate.name}</SelectItem>
+            ))}
           </Select>
           <div className="items-end flex min-h-full gap-2">
-            <Button onPress={getProduct} color="primary">
+            <Button onPress={fetchProducts} color="primary">
               <Search />
             </Button>
 
-            <Link href="/admin/product/edit/new" >
+            <Link href="/admin/product/edit/new">
               <Button color="primary">
                 <Plus />
               </Button>
             </Link>
 
-            <Button color="danger" isDisabled={![...selectedKeys].length && selectedKeys !== 'all'} onClick={deleteManyConfirmationDisclosure.onOpen}>
+            <Button color="danger" isDisabled={!selectedKeys.length && selectedKeys !== "all"} onClick={deleteManyConfirmationDisclosure.onOpen}>
               <Trash2 />
             </Button>
           </div>
@@ -281,76 +274,32 @@ const ProductCms = ({ categories = [] }) => {
             }
           >
             <TableHeader>
-              <TableColumn
-                key="id"
-                textValue="ID sản phẩm"
-                aria-label="ID sản phẩm"
-              >
-                ID
-              </TableColumn>
-              <TableColumn
-                key="name"
-                textValue="Tên sản phẩm"
-                aria-label="Tên sản phẩm"
-              >
-                Tên sản phẩm
-              </TableColumn>
-              <TableColumn key="slug" textValue="slug" aria-label="slug">
-                Slug
-              </TableColumn>
-              <TableColumn
-                key="productType"
-                textValue="productType"
-                aria-label="productType"
-              >
-                Loại
-              </TableColumn>
-              <TableColumn key="cate" textValue="cate" aria-label="cate">
-                Category
-              </TableColumn>
-              <TableColumn
-                key="subcate"
-                textValue="subcate"
-                aria-label="subcate"
-              >
-                Sub-category
-              </TableColumn>
-              <TableColumn
-                key="highlight"
-                textValue="highlight"
-                aria-label="active"
-              >
-                Nổi bật
-              </TableColumn>
-              <TableColumn key="active" textValue="active" aria-label="active">
-                Active
-              </TableColumn>
-              <TableColumn
-                key="actions"
-                textValue="actions"
-                width="100"
-              ></TableColumn>
+              <TableColumn key="id" textValue="ID sản phẩm" aria-label="ID sản phẩm">ID</TableColumn>
+              <TableColumn key="name" textValue="Tên sản phẩm" aria-label="Tên sản phẩm">Tên sản phẩm</TableColumn>
+              <TableColumn key="slug" textValue="slug" aria-label="slug">Slug</TableColumn>
+              <TableColumn key="productType" textValue="productType" aria-label="productType">Loại</TableColumn>
+              <TableColumn key="cate" textValue="cate" aria-label="cate">Category</TableColumn>
+              <TableColumn key="subcate" textValue="subcate" aria-label="subcate">Sub-category</TableColumn>
+              <TableColumn key="highlight" textValue="highlight" aria-label="active">Nổi bật</TableColumn>
+              <TableColumn key="active" textValue="active" aria-label="active">Active</TableColumn>
+              <TableColumn key="actions" textValue="actions" width="100"></TableColumn>
             </TableHeader>
             <TableBody
               items={products}
-              emptyContent={"Không có sản phẩm nào"}
+              emptyContent="Không có sản phẩm nào"
               isLoading={loadingState === "loading"}
               loadingContent={<Spinner label="Loading..." />}
             >
               {(item) => (
-                <TableRow key={item.id} data-selected="false">
-                  {(columnKey) => (
-                    <TableCell>{renderCell(item, columnKey)}</TableCell>
-                  )}
+                <TableRow key={item.id}>
+                  {(columnKey) => <TableCell>{renderCell(item, columnKey)}</TableCell>}
                 </TableRow>
               )}
             </TableBody>
           </Table>
         </div>
         <div className="pt-3">
-          <Link href="/admin/product/edit/new" className="float-right">
-            Thêm sản phẩm
-          </Link>
+          <Link href="/admin/product/edit/new" className="float-right">Thêm sản phẩm</Link>
         </div>
       </div>
     </>

3176bd6cf4f04106febbe8beddf79a3929d9539c
“ThanhTan” <nhthanhtan@gmail.com>
2025-04-09 21:53:33 +0700

    fix: role phase 2

--- src/middlewares/LoginHandler.js
+++ src/middlewares/LoginHandler.js
@@ -7,7 +7,7 @@ export const LoginHandler = (next) => {
     if (pathname === "/login") {
       const userCookie = request.cookies.get("user");
       const role = request.cookies.get("role");
-      if (userCookie && role) {
+      if (userCookie && role && role.value !== "undefined") {
         const url = request.nextUrl.clone();
         url.pathname = "/";
         return NextResponse.redirect(url);

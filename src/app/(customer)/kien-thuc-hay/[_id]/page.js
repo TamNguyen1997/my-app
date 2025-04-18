999578e6f7a350b3403f27ef64fa2d8b6163908f
TerriNguyen <terri.nguyen@thoughtworks.com>
2025-04-18 10:43:09 +0700

    Fix blog styling

--- src/app/(customer)/kien-thuc-hay/[_id]/page.js
+++ src/app/(customer)/kien-thuc-hay/[_id]/page.js
@@ -2,7 +2,7 @@ import { BlogDetail } from "@/components/blog/BlogDetail"
 import { notFound } from "next/navigation"
 
 export async function generateMetadata({ params }) {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_WORDPRESS_URL}/wp-json/wp/v2/posts/?slug=${params._id.trim()}&_embed`)
+  const res = await fetch(`${process.env.WORDPRESS_URL}/wp-json/wp/v2/posts/?slug=${params._id.trim()}&_embed`)
   if (!res.ok) {
     return {}
   }
@@ -11,11 +11,14 @@ export async function generateMetadata({ params }) {
   return {
     title: blog?.yoast_head_json?.og_title,
     description: blog?.yoast_head_json?.og_description,
+    alternates: {
+      canonical: `${process.env.NEXT_PUBLIC_DOMAIN}/kien-thuc-hay/${params._id}`
+    }
   }
 }
 
 const Information = async ({ params }) => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_WORDPRESS_URL}/wp-json/wp/v2/posts/?slug=${params._id}&_embed`)
+  const res = await fetch(`${process.env.WORDPRESS_URL}/wp-json/wp/v2/posts/?slug=${params._id}&_embed`)
   if (!res.ok) {
     notFound()
   }
@@ -25,11 +28,10 @@ const Information = async ({ params }) => {
     notFound()
   }
 
+  const relatedBlogRes = await fetch(`${process.env.WORDPRESS_URL}/wp-json/wp/v2/posts/?_embed&categories=${blog.categories?.join()}&exclude=${blog.id}&per_page=4&categories_exclude=${process.env.NEXT_PUBLIC_WORDPRESS_PRODUCT_CATEGORY_ID || ""}`)
+
   return (
-    <>
-      <link rel="canonical" href={`${process.env.NEXT_PUBLIC_DOMAIN}/kien-thuc-hay/${params._id}`} />
-      <BlogDetail slug={params._id.toString()} category="INFORMATION" blog={blog} />
-    </>
+    <BlogDetail slug={params._id.toString()} category="INFORMATION" blog={blog} relatedBlogs={relatedBlogRes.ok && (await relatedBlogRes.json())} />
   )
 };
 

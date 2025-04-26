48352ac7cf98731fa967b93f14c48a274833980e
TerriNguyen <terri.nguyen@thoughtworks.com>
2025-04-26 08:14:49 +0700

    update footer

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -42,6 +42,7 @@ const Footer = () => {
             { href: "/ho-tro/chinh-sach-bao-mat", text: "Bảo mật thông tin" },
             { href: "https://docs.google.com/forms/d/e/1FAIpQLSfbYULWGzXgkpcHs5LnCOpgB6inG3zWxR47ocWoZF-MGTfwlw/viewform", text: "Hợp tác bán hàng", external: true },
             { href: "/lien-he", text: "Liên hệ", external: true },
+            { href: "https://drive.google.com/drive/folders/1NJf3OdCXKGx23H9acc867UrQMGdq5vWM", text: "Tài liệu doanh nghiệp", external: true },
           ]} />
         </div>
 

00e5e438d2645cb42695de83ad0b051044c522c8
TamNguyen1997 <tam070397@gmail.com>
2025-05-12 22:17:03 +0700

    FIx format

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -49,78 +49,47 @@ async function generateSitemap() {
     "ho-tro/hinh-thuc-van-chuyen",
   ], 0.7, 'weekly'));
 
-  fs.writeFileSync(__dirname + '/public/sitemap-0.xml', `
-    <?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
-    <url><loc>${siteUrl}</loc><lastmod>2025-05-11T17:20:49.887Z</lastmod><changefreq>daily</changefreq><priority>1</priority></url>
-    </urlset>
-    `);
+  fs.writeFileSync(__dirname + '/public/sitemap-0.xml', `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
+<url><loc>${siteUrl}</loc><lastmod>2025-05-11T17:20:49.887Z</lastmod><changefreq>daily</changefreq><priority>1</priority></url>
+</urlset>`);
   fs.writeFileSync(__dirname + '/public/sitemap.xml',
-    `
-    <?xml version="1.0" encoding="UTF-8"?>
-    <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    <sitemap><loc>${siteUrl}/sitemap-0.xml</loc></sitemap>
-    <sitemap><loc>${siteUrl}/sitemap-san-pham.xml</loc></sitemap>
-    <sitemap><loc>${siteUrl}/sitemap-category.xml</loc></sitemap>
-    <sitemap><loc>${siteUrl}/sitemap-blog.xml</loc></sitemap>
-    <sitemap><loc>${siteUrl}/sitemap-ho-tro.xml</loc></sitemap>
-    </sitemapindex>
-    `
+    `<?xml version="1.0" encoding="UTF-8"?>
+<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<sitemap><loc>${siteUrl}/sitemap-0.xml</loc></sitemap>
+<sitemap><loc>${siteUrl}/sitemap-san-pham.xml</loc></sitemap>
+<sitemap><loc>${siteUrl}/sitemap-category.xml</loc></sitemap>
+<sitemap><loc>${siteUrl}/sitemap-blog.xml</loc></sitemap>
+<sitemap><loc>${siteUrl}/sitemap-ho-tro.xml</loc></sitemap>
+</sitemapindex>`
   )
 }
 
 generateSitemap();
 function convertToXmlProductSiteMap(products, priority, changefreq) {
   return `<?xml version="1.0" encoding="UTF-8"?>
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/${changefreq}">
-  ${products.map(p => `
-    <url>
-      <loc>${siteUrl}/${p.subCate.slug}/${p.slug}</loc>
-      <lastmod>${p.updatedAt.toISOString()}</lastmod>
-      <changefreq>${priority}</changefreq>
-      <priority>${changefreq}</priority>
-    </url>`).join('')}
-  </urlset>`;
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/${changefreq}">${products.map(p => `
+<url><loc>${siteUrl}/${p.subCate.slug}/${p.slug}</loc><lastmod>${p.updatedAt.toISOString()}</lastmod><changefreq>${priority}</changefreq><priority>${changefreq}</priority></url>`).join('')}
+</urlset>`;
 }
 
 function convertToXmlCategoriesSiteMap(categories, priority, changefreq) {
   return `<?xml version="1.0" encoding="UTF-8"?>
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/${changefreq}">
-  ${categories.map(cate => `
-    <url>
-      <loc>${siteUrl}/${cate.slug}</loc>
-      <lastmod>${cate.updatedAt.toISOString()}</lastmod>
-      <changefreq>${priority}</changefreq>
-      <priority>${changefreq}</priority>
-    </url>`).join('')}
-  </urlset>`;
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/${changefreq}">${categories.map(cate => `
+<url><loc>${siteUrl}/${cate.slug}</loc><lastmod>${cate.updatedAt.toISOString()}</lastmod><changefreq>${priority}</changefreq><priority>${changefreq}</priority></url>`).join('')}
+</urlset>`;
 }
 
 function convertToXmlBLogsSiteMap(blogs, priority = 0.9, changefreq) {
   return `<?xml version="1.0" encoding="UTF-8"?>
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/${changefreq}">
-  ${blogs.map(blog => `
-    <url>
-      <loc>${siteUrl}/${blog.categories.includes(parseInt(process.env.NEXT_PUBLIC_WORDPRESS_POST_NEWS_ID))
-      ? "tin-tuc"
-      : "kien-thuc-hay"
-    }/${blog.slug}</loc>
-      <lastmod>${blog.modified || new Date().toISOString()}</lastmod>
-      <changefreq>${priority}</changefreq>
-      <priority>${changefreq}</priority>
-    </url>`).join('')}
-  </urlset>`;
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/${changefreq}">${blogs.map(blog => `
+<url><loc>${siteUrl}/${blog.categories.includes(parseInt(process.env.NEXT_PUBLIC_WORDPRESS_POST_NEWS_ID)) ? "tin-tuc" : "kien-thuc-hay"}/${blog.slug}</loc><lastmod>${blog.modified || new Date().toISOString()}</lastmod><changefreq>${priority}</changefreq><priority>${changefreq}</priority></url>`).join('')}
+</urlset>`;
 }
 
 function convertToXmlAboutUsSiteMap(urls, priority = 0.7, changefreq = "weekly") {
   return `<?xml version="1.0" encoding="UTF-8"?>
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/${changefreq}">
-  ${urls.map(p => `
-    <url>
-      <loc>${siteUrl}/${urls}</loc>
-      <lastmod>${new Date().toISOString()}</lastmod>
-      <changefreq>${priority}</changefreq>
-      <priority>${changefreq}</priority>
-    </url>`).join('')}
-  </urlset>`;
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/${changefreq}">${urls.map(p => `
+<url><loc>${siteUrl}/${urls}</loc><lastmod>${new Date().toISOString()}</lastmod><changefreq>${priority}</changefreq><priority>${changefreq}</priority></url>`).join('')}
+</urlset>`;
 }

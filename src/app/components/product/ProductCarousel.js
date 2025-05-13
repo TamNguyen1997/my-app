3c38798a264c475884ba36ce054bafd501e81b17
TamNguyen1997 <tam070397@gmail.com>
2025-05-13 22:02:29 +0700

    Fix responsive

--- src/app/components/product/ProductCarousel.js
+++ src/app/components/product/ProductCarousel.js
@@ -17,11 +17,11 @@ const defaultResponsive = {
     items: 4,
   },
   tablet: {
-    breakpoint: { max: 1024, min: 350 },
+    breakpoint: { max: 1024, min: 750 },
     items: 3,
   },
   mobile: {
-    breakpoint: { max: 350, min: 0 },
+    breakpoint: { max: 750, min: 0 },
     items: 2,
   },
 };

8ef5eaffe3b0382b69ee6610da47b08c3a69a579
TamNguyen1997 <tam070397@gmail.com>
2025-05-14 22:15:00 +0700

    Fix filter range

--- src/app/(customer)/thuong-hieu-rubbermaid/page.js
+++ src/app/(customer)/thuong-hieu-rubbermaid/page.js
@@ -1,5 +1,7 @@
 import BrandPage from "@/components/BrandPage";
 import { WEBSITE_SCHEMA, ORGANIZATION_SCHEMA, getBreadcrumbSchema, getBrandSchema } from "@/lib/schema"
+import { db } from "@/app/db"
+import { notFound } from "next/navigation";
 
 export const metadata = {
   title: 'Thương hiệu Rubbermaid',
@@ -22,12 +24,36 @@ const jsonLdSchema = {
   ]
 }
 
-export default function Page() {
+export default async function Page() {
+  const brand = await db.brand.findFirst({ where: { slug: 'thuong-hieu-rubbermaid' } })
+  if (!brand) {
+    notFound()
+  }
+
+  const filters = await db.filter.findMany({
+    where: {
+      active: true,
+      filterValue: {
+        some: {
+          brand_on_filter_value: {
+            some: {
+              brandId: brand.id,
+            }
+          },
+          active: true
+        }
+      }
+    },
+    include: {
+      filterValue: true
+    }
+  })
+
   return (<>
     <script
       type="application/ld+json"
       dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLdSchema) }}
     />
-    <BrandPage brand="thuong-hieu-rubbermaid" bg="bg-[url(/brand/banner/1440_290_Banner_RBM.png)]" />
+    <BrandPage brand="thuong-hieu-rubbermaid" bg="bg-[url(/brand/banner/1440_290_Banner_RBM.png)]" filter={filters} />
   </>)
 }
--- src/app/components/BrandPage.js
+++ src/app/components/BrandPage.js
@@ -3,14 +3,14 @@
 import Brand from "@/app/components/product/Brand";
 import { usePathname } from 'next/navigation'
 
-const BrandPage = ({ brand, bg }) => {
+const BrandPage = ({ brand, bg, filters }) => {
   const [_, filter] = usePathname().split("#")
   return (<>
     <div className="flex flex-col items-center justify-center w-full xl:h-96 lg:h-72 md:h-60 h-32 bg-cover bg-center bg-no-repeat">
       <div className={`flex flex-col items-center justify-center w-full h-full bg-no-repeat bg-cover ${bg}`}>
       </div>
     </div>
-    <Brand params={brand} productFilter={filter} />
+    <Brand params={brand} productFilter={filter} filters={filters} />
   </>
   )
 };

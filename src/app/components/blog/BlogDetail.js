29940f2fe1229b5d5e8245e8a26f2a590dc59a15
TamNguyen1997 <tam070397@gmail.com>
2025-08-01 14:06:20 +0700

    update stuff

--- src/app/components/blog/BlogDetail.js
+++ src/app/components/blog/BlogDetail.js
@@ -9,6 +9,7 @@ import "./BlogDetail.css"
 
 const BlogContent = ({ blog }) => {
   return (<>
+    <link rel='stylesheet' id='wp-block-library-css' href='https://dcvs.shop/wordpress/wp-includes/css/dist/block-library/common.min.css?ver=6.8.2' media='all' />
     <motion.div
       initial={{ x: -50, opacity: 0 }}
       whileInView={{ x: 0, opacity: 1 }}
@@ -24,15 +25,12 @@ const BlogContent = ({ blog }) => {
       whileInView={{ y: 0, opacity: 1 }}
       transition={{ duration: 0.7, delay: 0.3 }}
       viewport={{ once: true }}
-      className={`
-                [&_a]:text-primary
-                [&_h2]:mt-[1.25em]
-                [&_p]:my-[1.125em]
-                max-w-full prose blog-content
-              `}
+      className={`blog-content`}
       style={{ "--tw-prose-bullets": "currentColor" }}
     >
-      {blog.content ? parse(blog.content.rendered || blog.content) : ""}
+      <div class="entry-content">
+        {blog.content ? parse(blog.content.rendered || blog.content) : ""}
+      </div>
     </motion.div>
   </>)
 }

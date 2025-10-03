42bdc76b6b4e476a858b6593b75608d77436b46e
TerriNguyen <terri.nguyen@myob.com>
2025-10-03 23:31:50 +0700

    update stuff

--- next.config.js
+++ next.config.js
@@ -26,6 +26,12 @@ const nextConfig = {
           { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
           { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Location" },
         ]
+      },
+      {
+        source: "/_next/static/:path*",
+        headers: [
+          { key: "X-Robots-Tag", value: "noindex" },
+        ]
       }
     ]
   },

08795988793d1858cd6c57127af78748d09a1f2e
TerriNguyen <terri.nguyen@thoughtworks.com>
2025-12-24 09:43:04 +0700

    update stuff

--- src/app/(admin)/admin/product/edit/[id]/default.js
+++ src/app/(admin)/admin/product/edit/[id]/default.js
@@ -14,6 +14,7 @@ import { product_type } from "@prisma/client";
 import { toast, ToastContainer } from "react-toastify";
 import { useSearchParams } from 'next/navigation';
 import ProductBundle from "../../../../../components/admin/ui/product/ProductBundle";
+import slugify from "slugify"
 
 export const ProductContext = createContext();
 
